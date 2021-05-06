c9764155e03088a9fd7e556997a1440b73da834b
unknown <iryna.chycherska.itis.2020@lpnu.ua>
2021-05-06 17:23:14 +0300

    6.1

---
+++ UnitTest6.1/UnitTest6.1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"../6.1/Array.h"
+#include"../6.1/Array.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest61
+{
+	TEST_CLASS(UnitTest61)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Array Test = Array(49);
+			for (int i = 0; i < 49; i++)
+				Test[i] = 0;
+			Test[18] = -50;
+
+			Assert::AreEqual(Test.Min(), -50.);
+		}
+	};
+}
